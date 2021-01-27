a22b7d2d031acecd81fde895313c2878ef5f14c4
Roshan Kumar <roshan10kushwaha@gmail.com>
2021-01-27 23:32:43 +0530

    Added Checking Even Odd Program

---
+++ CheckingEvenOdd.py
@@ -0,0 +1,7 @@
+n = int(input ('Enter the number you want to check : '))
+if n == 0:
+    print('The number is zero')
+if n > 0:
+    print('The number is positive')
+if n < 0:
+    print('The number is negative')
