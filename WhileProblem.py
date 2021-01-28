77e292163f806bf40f6c0c1841095a427aa82d46
Roshan Kumar <roshan10kushwaha@gmail.com>
2021-01-28 08:20:09 +0530

    Added While Program

---
+++ WhileProblem.py
@@ -0,0 +1,27 @@
+r = int(input('Enter  r :'))
+
+for i in range (1,r+1):
+    for j in range(1,i+i):
+        print('*', end = ' ')
+    print()
+
+
+
+
+r = int(input('Enter  r :'))
+
+for i in range (1,r+1):
+    for j in range(1,i+1):
+        print(j, end = ' ')
+    print()
+
+
+
+n = int(input('Enter  r :'))
+
+for i in range (1,n+1):
+    for j in range(1,n-i+1):
+        print(' ', end = ' ')
+    for m in range (1,2*i - 1+1):ww
+        print('*', end = ' ')
+    print()
